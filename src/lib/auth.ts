b488e2c7601bd119eae984cfdfa8195213e1ad90
Mac Rountree <a219206225@unison.mx>
2023-12-04 09:03:08 -0700

    Fix bug in login functionality video 1:05:00

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+// Aqui va la lógica de auth
+
+import { NextAuthOptions } from 'next-auth';
+import { db } from './db';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/sign-in',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
